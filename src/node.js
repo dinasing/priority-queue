645fc876ce7a0d63dd9e313a06c98e6268e8e7e2
Dina Singatulina <dina.sing1212@gmail.com>
2019-10-14 12:34:29 +0300

    feat: add removeChild, appendChild, remove

--- src/node.js
+++ src/node.js
@@ -8,19 +8,43 @@ class Node {
 	}
 
 	appendChild(node) {
-
+		if(!this.left) {
+			this.left = node;
+			this.left.parent = this;
+		}
+		else if (!this.right) {
+			this.right = node;
+			this.right.parent = this;
+		}
+		
+		
 	}
 
 	removeChild(node) {
-
+		if (node == this.left) {
+			this.left.parent = null;
+			this.left = null;
+		}
+		else if (node == this.right) {
+			this.right.parent = null;
+			this.right = null;
+		}
+		else throw "error";
 	}
 
 	remove() {
-
+		if (this.parent) {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if (this.parent) {
+			let t;
+			t = this.parent.parent;
+			this.parent.parent = this;
+			this.parent = t;
+		}
 	}
 }
 
